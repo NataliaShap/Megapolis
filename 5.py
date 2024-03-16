69043e4b01eb222021b55445757db1a573b6e7cb
ShaposhnikovaNatalia <saposnikovanatalia712@gmail.com>
2024-03-16 15:16:36 +0300

    update

--- 5.py
+++ 5.py
@@ -1,4 +1,11 @@
 import csv
 
-with open('vacancy.csv', encoding='utf-8') as file:
+with open('vacancy.csv', encoding='utf-8') as file, open('vacancy_hash.csv', 'w', encoding='utf-8') as new_h:
     data = list(csv.reader(file, delimiter=';'))
+    res = csv.writer(new_h, delimiter=';')
+
+    res.writerow(data[0])
+    new = {}
+    for stroka in data[1:]:
+        new = {stroka[-1]:[stroka[0], stroka[1], stroka[2], stroka[3]]}
+    res.writerow(new)
