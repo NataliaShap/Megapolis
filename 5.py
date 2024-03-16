824c8fc0d6cbf9ecf8eaf0ea7c60310dc3335ae2
ShaposhnikovaNatalia <saposnikovanatalia712@gmail.com>
2024-03-16 15:19:25 +0300

    update

--- 5.py
+++ 5.py
@@ -7,5 +7,5 @@ with open('vacancy.csv', encoding='utf-8') as file, open('vacancy_hash.csv', 'w'
     res.writerow(data[0])
     new = {}
     for stroka in data[1:]:
-        new = {stroka[-1]:[stroka[0], stroka[1], stroka[2], stroka[3]]}
+        new = {stroka[-1]:stroka[-2]}
     res.writerow(new)
